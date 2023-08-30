af01df7df2eced82e7a0ccbf0d00a56834a0df60
Stanislas Marçais / Knupel <stanislasiii@gmail.com>
2023-08-30 18:56:02 +0200

    impossible de récupérer les données sereinement

--- pages/database.tsx
+++ pages/database.tsx
@@ -2,11 +2,34 @@
 import { useState } from "react";
 // import Link from 'next/link';
 import type { ReactElement } from 'react'
+import { PrismaClient } from '@prisma/client'
 
 // APP
 import type { NextPageWithLayout } from './_app';
 import Layout from '../components/layout';
 import { AddAnimalForm } from '../components/form';
+
+// CALL DATA
+const prisma = new PrismaClient();
+
+// https://nextjs.org/docs/app/building-your-application/data-fetching/patterns
+async function getData() {
+  const res = await fetch('https://nextreactlab.netlify.app/api/animal')
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  console.log("res", res);
+  return res.json()
+}
+ 
+export async function GetData() {
+  const data = await getData(); 
+  return <>{data}</>
+}
+
+
+
+
 
 
 
@@ -18,6 +41,7 @@ import { AddAnimalForm } from '../components/form';
 const Page: NextPageWithLayout = () => {
   return <>
     <AddAnimalForm/>
+    {/* <GetData/> */}
   </>
 }
 
